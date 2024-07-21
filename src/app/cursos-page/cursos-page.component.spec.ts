e04a9dea136e00cfe1148eb6cb03e15126a15f7d
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2024-07-20 21:33:36 -0500

    Update cursos-page.component.spec.ts

--- src/app/cursos-page/cursos-page.component.spec.ts
+++ src/app/cursos-page/cursos-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CursosPageComponent } from './cursos-page.component';
+import { CursosPageComponent} from './cursos-page.component';
 
 describe('CursosPageComponent', () => {
   let component: CursosPageComponent;
