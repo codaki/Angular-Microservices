8d6f317e490571f5af4cde88c2f926679ef1c44c
Chaps0218 <140579994+Chaps0218@users.noreply.github.com>
2024-07-21 14:59:01 -0500

    Revert "Subir Microservicios"
    This reverts commit 34c4374b7148f4132f7808b1d007a0dd4c1965db.

--- MicroServicios/msvc-cursos/src/main/java/com/espe/msvc/cursos/repositories/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.msvc.cursos.repositories;
-
-import com.espe.msvc.cursos.models.entity.Curso;
-import org.springframework.data.repository.CrudRepository;
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-
-}
