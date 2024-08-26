58780dc02ce3ba85a2183d92c0e359b118d2636b
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2024-08-25 20:26:30 -0500

    Update main.ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,15 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 import { routes } from './app/app.routes';
-
+import { KeycloakService } from './app/guard/keycloak.service';
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       RouterModule.forRoot(routes),
       HttpClientModule // Agrega HttpClientModule aquí
-    )
+    ),
+    {
+      provide: APP_INITIALIZER,
+      deps: [KeycloakService],
+      useFactory: kcFactory,
+      multi: true
+    },
   ]
 }).catch((err) => console.error(err));
