797746b7ed0f0de88c7e1c788c7526d1485d7853
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2024-08-25 20:26:12 -0500

    Update app.routes.ts

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { EstudiantesPageComponent } from './estudiantes-page/estudiantes-page.co
 import { MainPageComponent } from './main-page/main-page.component';
 import { CursosPageComponent } from './cursos-page/cursos-page.component';
 import { RelacionPageComponent } from './relacion-page/relacion-page.component';
+//import { AuthGuard } from './guard/keycloak.service';
 
 export const routes: Routes = [
+    //{ path: '', component: ContentComponent , canActivate: [AuthGuard]},
+    //{ path: '**', redirectTo: '' },
+    
     { path: 'usuarios', component: EstudiantesPageComponent },
-    { path: 'cursos', component: CursosPageComponent },
+    { path: 'cursos', component: CursosPageComponent  },
     { path: 'relacion', component: RelacionPageComponent },
-    { path: '', component: MainPageComponent } // Ruta por defecto
+    { path: '', component: MainPageComponent  } // Ruta por defecto
 ];
