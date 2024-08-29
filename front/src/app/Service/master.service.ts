dcea35ca57c16513d72f7f6ff69470d20e4600ca
Christopher Iza <cdiza5@espe.edu.ec>
2024-08-29 00:04:31 -0500

    Modificacion del front con oauth

--- front/src/app/Service/master.service.ts
+++ front/src/app/Service/master.service.ts
@@ -1,7 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ICursoUsuario, CCursoCompleto, ICursoCompleto, IUsuario, CUsuario, ICurso, CCurso } from '../model/usuario';
+import {
+  CCurso,
+  CCursoCompleto,
+  CUsuario,
+  ICurso,
+  ICursoCompleto,
+  ICursoUsuario,
+  IUsuario,
+} from '../model/usuario';
 
 @Injectable({
   providedIn: 'root',
@@ -9,52 +17,90 @@ import { ICursoUsuario, CCursoCompleto, ICursoCompleto, IUsuario, CUsuario, ICur
 export class MasterService {
   apiUrlUsuario: string = 'http://localhost:8001/';
   apiUrlCurso: string = 'http://localhost:8002/';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token'); // Obtén el token del localStorage
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`, // Añade el token a los headers
+    });
+  }
 
   getCursos(): Observable<ICurso[]> {
-    return this.http.get<ICurso[]>(this.apiUrlCurso + 'getCursos');
+    return this.http.get<ICurso[]>(this.apiUrlCurso + 'getCursos', {
+      headers: this.getHeaders(),
+    });
   }
+
   addCurso(curso: CCurso): Observable<CCurso> {
-    return this.http.post<CCurso>(this.apiUrlCurso + 'saveCurso', curso);
+    return this.http.post<CCurso>(this.apiUrlCurso + 'saveCurso', curso, {
+      headers: this.getHeaders(),
+    });
   }
+
   deleteCurso(id: number): Observable<ICurso> {
-    return this.http.delete<ICurso>(this.apiUrlCurso + 'eliminarCurso/' + id);
+    return this.http.delete<ICurso>(this.apiUrlCurso + 'eliminarCurso/' + id, {
+      headers: this.getHeaders(),
+    });
   }
+
   updateCurso(curso: CCurso, id: number): Observable<CCurso> {
-    return this.http.put<CCurso>(this.apiUrlCurso + 'modificarCurso/' + id, curso);
+    return this.http.put<CCurso>(
+      this.apiUrlCurso + 'modificarCurso/' + id,
+      curso,
+      { headers: this.getHeaders() }
+    );
   }
+
   asignarUsuario(idCurso: number, usuario: IUsuario): Observable<any> {
     const url = `${this.apiUrlCurso}asignarUsuario/${idCurso}`;
-    return this.http.put<any>(url, usuario, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
+    return this.http.put<any>(url, usuario, { headers: this.getHeaders() });
   }
+
   desasignarUsuario(idCurso: number, usuario: IUsuario): Observable<any> {
     const url = `${this.apiUrlCurso}eliminarUsuario/${idCurso}`;
-    return this.http.put<any>(url, usuario, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+    return this.http.put<any>(url, usuario, { headers: this.getHeaders() });
+  }
+
+  getCursosCompletos(): Observable<ICursoCompleto[]> {
+    return this.http.get<ICursoCompleto[]>(this.apiUrlCurso + 'getCursos', {
+      headers: this.getHeaders(),
     });
   }
-  getCursosCompletos(): Observable<ICursoCompleto[]> {
-    return this.http.get<ICursoCompleto[]>(this.apiUrlCurso + 'getCursos');
-  }
+
   getUsuariosNoMatriculados(idCurso: number): Observable<IUsuario[]> {
-    return this.http.get<IUsuario[]>(this.apiUrlCurso + 'usuarioIds/' + idCurso);
+    return this.http.get<IUsuario[]>(
+      this.apiUrlCurso + 'usuarioIds/' + idCurso,
+      { headers: this.getHeaders() }
+    );
   }
+
   getUsuarios(): Observable<IUsuario[]> {
-    return this.http.get<IUsuario[]>(this.apiUrlUsuario + 'getUsers');
+    return this.http.get<IUsuario[]>(this.apiUrlUsuario + 'getUsers', {
+      headers: this.getHeaders(),
+    });
   }
+
   addUsuario(usuario: CUsuario): Observable<CUsuario> {
-    return this.http.post<CUsuario>(this.apiUrlUsuario + 'saveUser', usuario);
+    return this.http.post<CUsuario>(this.apiUrlUsuario + 'saveUser', usuario, {
+      headers: this.getHeaders(),
+    });
   }
+
   deleteUsuario(id: number): Observable<IUsuario> {
-    return this.http.delete<IUsuario>(this.apiUrlUsuario + 'eliminarUser/' + id);
+    return this.http.delete<IUsuario>(
+      this.apiUrlUsuario + 'eliminarUser/' + id,
+      { headers: this.getHeaders() }
+    );
   }
+
   updateUsuario(usuario: CUsuario, id: number): Observable<CUsuario> {
-    return this.http.put<CUsuario>(this.apiUrlUsuario + 'modificarUser/' + id, usuario);
+    return this.http.put<CUsuario>(
+      this.apiUrlUsuario + 'modificarUser/' + id,
+      usuario,
+      { headers: this.getHeaders() }
+    );
   }
 }
--- front/src/app/main-page/main-page.component.ts
+++ front/src/app/main-page/main-page.component.ts
@@ -1,14 +1,74 @@
+// main-page.component.ts
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { EstudiantesPageComponent } from '../estudiantes-page/estudiantes-page.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
-  standalone: true,
-  imports: [EstudiantesPageComponent],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
-  
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  constructor(private http: HttpClient, private router: Router) {}
 
+  private authenticate() {
+    const url =
+      'http://localhost:8080/realms/estudiantes-cursos/protocol/openid-connect/token';
+    const body = new URLSearchParams();
+    body.set('grant_type', 'password');
+    body.set('client_id', 'backend');
+    body.set('username', 'admin2');
+    body.set('password', 'admin2');
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post<any>(url, body.toString(), { headers: headers });
+  }
+
+  navigateToUsuarios() {
+    this.authenticate().subscribe(
+      (response) => {
+        // Almacena el access_token en el localStorage
+        localStorage.setItem('access_token', response.access_token);
+        console.log('Token almacenado con éxito:', response.access_token);
+        // Navega a la página de usuarios
+        this.router.navigate(['/usuarios']);
+      },
+      (error) => {
+        console.error('Error en la autenticación:', error);
+      }
+    );
+  }
+
+  navigateToCursos() {
+    this.authenticate().subscribe(
+      (response) => {
+        // Almacena el access_token en el localStorage
+        localStorage.setItem('access_token', response.access_token);
+        console.log('Token almacenado con éxito:', response.access_token);
+        // Navega a la página de cursos
+        this.router.navigate(['/cursos']);
+      },
+      (error) => {
+        console.error('Error en la autenticación:', error);
+      }
+    );
+  }
+
+  navigateToRelacion() {
+    this.authenticate().subscribe(
+      (response) => {
+        // Almacena el access_token en el localStorage
+        localStorage.setItem('access_token', response.access_token);
+        console.log('Token almacenado con éxito:', response.access_token);
+        // Navega a la página de relaciones
+        this.router.navigate(['/relacion']);
+      },
+      (error) => {
+        console.error('Error en la autenticación:', error);
+      }
+    );
+  }
 }
